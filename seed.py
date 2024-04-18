3ab338f3db4c0d48dcd69346e640c02144cdf604
Andy Patari <andy.patari@gmail.com>
2024-04-18 14:10:57 -0400

    seed creates inventory test collection

--- seed.py
+++ seed.py
@@ -1,14 +1,44 @@
-from dotenv import load_dotenv
-import os
 
 from config import client
 
-if __name__ == '__main--':
-    try:
-        client.admin.command('ping')
-        print("Pinged your deployment. You successfully connected to MongoDB!")
-    except Exception as e:
-        print(e)
+
+print('adding inventory')    
+db = client.PRACTICE_DB
+db.inventory.insert_many(
+    [
+        {
+            "item": "journal",
+            "qty": 25,
+            "size": {"h": 14, "w": 21, "uom": "cm"},
+            "status": "A",
+        },
+        {
+            "item": "notebook",
+            "qty": 50,
+            "size": {"h": 8.5, "w": 11, "uom": "in"},
+            "status": "A",
+        },
+        {
+            "item": "paper",
+            "qty": 100,
+            "size": {"h": 8.5, "w": 11, "uom": "in"},
+            "status": "D",
+        },
+        {
+            "item": "planner",
+            "qty": 75,
+            "size": {"h": 22.85, "w": 30, "uom": "cm"},
+            "status": "D",
+        },
+        {
+            "item": "postcard",
+            "qty": 45,
+            "size": {"h": 10, "w": 15.25, "uom": "cm"},
+            "status": "A",
+        },
+    ]
+)
+print(db.inventory.find({}))
 
     
 
