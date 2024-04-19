37688084aec53866f06dfede2eee3f5273059c6b
Andy Patari <andy.patari@gmail.com>
2024-04-19 09:45:30 -0400

    create route using mongodb object id

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ from config import app, client
 
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
+from bson.objectid import ObjectId
+
 load_dotenv()
 
-
+db = client.PRACTICE_DB
 
 # Send a ping to confirm a successful connection
 try:
@@ -21,6 +23,24 @@ except Exception as e:
 @app.route('/')
 def home():
     return jsonify({"data": "hello world"})
+
+@app.route('/inv/<string:page_id>')
+def page(page_id):
+    print(page_id)
+    cursor = db.inventory.find({'_id': ObjectId(page_id)})
+    results = list(cursor)
+    if len(results) == 0:
+        return {"errors": "Not found"}, 404
+    else:
+        dict = results[0]
+        response_body = {
+            'item': dict['item'],
+            'qty': dict['qty'],
+            "id": str(ObjectId(dict['_id']))
+        }
+        
+        return response_body, 200
+
 
 if __name__ == '__main__': 
   
--- queryPrac.py
+++ queryPrac.py
@@ -1,11 +1,14 @@
 from config import client
+from bson.objectid import ObjectId
 
 
 
 db = client.PRACTICE_DB
 
 cursor = db.inventory.find({
-    "status":"D"
+    "_id": ObjectId("66216367815b33209271c615")
 })
+# print(len(list(cursor)))
 for item in cursor:
-    print(item['item'])
+    
+    print(str(item['_id']))
